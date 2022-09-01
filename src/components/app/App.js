aefa993d92637f23ae804a1cc1de90174b236d69
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-09-01 12:22:53 +0300

    feat: add Header, vars.css; ref: App styles

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './App.module.css';
+import Header from '../header/Header';
 
 function App() {
   return (
-    <div className={styles.page}>
-      <header className="App-header">
-      </header>
+    <div>
+      <Header />
+      <div className={styles.content}/>
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import logoPath from '../../images/logo.svg'
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>Погода</h1>
+      <img className={styles.logo} src={logoPath} />
+    </header>
+  )
+}
+
+export default Header;
