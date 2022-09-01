55b01d59aa5a695e5530c92d735705313a089ca4
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-09-01 15:48:39 +0300

    fix: add alt to logo, replace body style to app.css

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import styles from './App.module.css';
 import Header from '../header/Header';
 import SearchBar from '../searchBar/SearchBar';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const city = useSelector(state => state.cities.city);
   return (
     <div>
       <Header />
       <div className={styles.content}>
         <SearchBar />
+        <p>{city ? city.label : ''}</p>
       </div>
     </div>
   );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>Погода</h1>
-      <img className={styles.logo} src={logoPath} />
+      <img className={styles.logo} src={logoPath} alt='Логотип' />
     </header>
   )
 }
