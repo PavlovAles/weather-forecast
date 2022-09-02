d4408278527d94123415d8a4b88827d8a5514e06
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-09-02 13:03:03 +0300

    feat: add Spinner component

---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import spinnerPath from '../../images/spinner.svg';
+import styles from './Spinner.module.css';
+
+function Spinner() {
+  return (
+    <img className={styles.spinner} src={spinnerPath} alt='Иконка спиннер' />
+  )
+}
+
+export default Spinner;
