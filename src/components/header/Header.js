d2459972967c2f263682203ef6e0a2366cdabe6c
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-09-05 12:07:54 +0300

    feat: add home link

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import logoPath from '../../images/logo.svg'
 import SearchBar from '../searchBar/SearchBar';
 import styles from './Header.module.css';
@@ -5,10 +6,12 @@ import styles from './Header.module.css';
 function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logoContainer}>
-        <h1 className={styles.title}>Погода</h1>
-        <img className={styles.logo} src={logoPath} alt='Логотип' />
-      </div>
+      <NavLink className={styles.link} to='/'>
+        <div className={styles.logoContainer}>
+          <h1 className={styles.title}>Погода</h1>
+          <img className={styles.logo} src={logoPath} alt='Логотип' />
+        </div>
+      </NavLink>
       <SearchBar />
     </header>
   )
