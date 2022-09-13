9d6ec0407f24a6fb8a74306c2353e7e62a5e27f5
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-09-13 11:41:01 +0300

    feat: footer styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,12 +5,14 @@ import logoPath from '../../images/openweather.jpeg';
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <img
-        className={styles.logo}
-        src={logoPath}
-        alt='Логотип OpenWeatherAPI'
-      />
-      <p className={styles.text}>Алесь Павлов, 2022г.</p>
+      <div className={styles.container}>
+        <img
+          className={styles.logo}
+          src={logoPath}
+          alt='Логотип OpenWeatherAPI'
+        />
+        <p className={styles.text}>Алесь Павлов, 2022г.</p>
+      </div>
     </footer>
   );
 }
